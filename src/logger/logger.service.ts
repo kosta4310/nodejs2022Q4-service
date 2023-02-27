558b93f63be3c2d5a2e70a942373bc793ce9ad79
Pavlov Kostyantyn <realanka77@gmail.com>
2023-02-27 11:42:46 +0200

    feat: add log level

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -4,13 +4,14 @@ import {
   ConsoleLogger,
   ConsoleLoggerOptions,
 } from '@nestjs/common';
+import { getLogLevels } from './utils/getLogLevel';
 
 @Injectable({ scope: Scope.TRANSIENT })
 export class MyLogger extends ConsoleLogger {
   constructor(context: string, options: ConsoleLoggerOptions) {
     super(context, {
       ...options,
-      logLevels: ['error', 'log'],
+      logLevels: getLogLevels(),
     });
   }
 }
